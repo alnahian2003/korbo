21477c72de4f0e0b7ae032929e41fb5e6d118fa0
Al Nahian <61485238+alnahian2003@users.noreply.github.com>
2020-11-22 22:04:26 +0600

    CHECKBOX ADDED INSTEAD ICON AND TASKS ARE NOW INTERACTIVE

--- app.js
+++ app.js
@@ -2,114 +2,128 @@
 
 // Main Function
 let tasker = {
-    construct: function () {
-        tasker.selectElements();
-        tasker.bindEvents();
-        tasker.scanTaskList();
-    },
-    selectElements: function () {
-        this.taskInput = document.getElementById("input");
-        this.taskList = document.getElementById("tasksContainer");
-        this.taskListChildren = this.taskList.children;
-        this.submitBtn = document.getElementById("submit");
-    },
-    buildTask: function () {
-        let taskListItem, taskMarkContainer, taskCompleted, taskValueContainer, taskValue, taskDeleteContainer, taskDelete;
-        taskListItem = document.createElement("li");
+  construct: function () {
+    tasker.selectElements();
+    tasker.bindEvents();
+    tasker.scanTaskList();
+  },
+  selectElements: function () {
+    this.taskInput = document.getElementById("input");
+    this.taskList = document.getElementById("tasksContainer");
+    this.taskListChildren = this.taskList.children;
+    this.submitBtn = document.getElementById("submit");
+  },
+  buildTask: function () {
+    let taskListItem,
+      taskMarkContainer,
+      taskCompleted,
+      taskValueContainer,
+      taskValue,
+      taskDeleteContainer,
+      taskDelete;
+    taskListItem = document.createElement("li");
 
-        // chekmark
-        taskMarkContainer = document.createElement("span");
-        taskMarkContainer.setAttribute("class", "mark");
-        taskCompleted = document.createElement("i");
-        taskCompleted.setAttribute("class", "las la-check-circle");
-        taskMarkContainer.appendChild(taskCompleted);
+    // chekmark
+    taskMarkContainer = document.createElement("span");
+    taskMarkContainer.setAttribute("class", "mark");
 
-        // task value
-        taskValueContainer = document.createElement("div");
-        taskValueContainer.setAttribute("class", "list");
-        taskValue = document.createElement("p");
-        taskValue.innerHTML = this.taskInput.value;
-        taskValueContainer.appendChild(taskValue);
+    // New Check Box Added Instead an Icon in v1.1
+    taskCompleted = document.createElement("input");
+    taskCompleted.setAttribute("type", "checkbox");
+    taskCompleted.setAttribute("class", "checkbox");
 
-        // delete
-        taskDeleteContainer = document.createElement("span");
-        taskDeleteContainer.setAttribute("class", "delete");
-        taskDelete = document.createElement("i");
-        taskDelete.setAttribute("class", "las la-trash-alt");
-        taskDeleteContainer.appendChild(taskDelete);
+    taskMarkContainer.appendChild(taskCompleted);
 
-        // append elementS to tasklist
-        taskListItem.appendChild(taskMarkContainer);
-        taskListItem.appendChild(taskValueContainer);
-        taskListItem.appendChild(taskDeleteContainer);
+    // task value
+    taskValueContainer = document.createElement("div");
+    taskValueContainer.setAttribute("class", "list");
+    taskValue = document.createElement("p");
+    taskValue.innerHTML = this.taskInput.value;
+    taskValueContainer.appendChild(taskValue);
 
-        // add task to tasklist
-        this.taskList.appendChild(taskListItem);
-    },
+    // delete
+    taskDeleteContainer = document.createElement("span");
+    taskDeleteContainer.setAttribute("class", "delete");
+    taskDelete = document.createElement("i");
+    taskDelete.setAttribute("class", "las la-trash-alt");
+    taskDeleteContainer.appendChild(taskDelete);
 
-    addTask: function () {
-        let taskValue = this.taskInput.value;
-        const alertMessage = document.querySelector(".alert");
-        const placeholder = document.querySelector(".placeholder");
-        if (taskValue === "") {
-            alertMessage.style.display = "block";
-            // Hide Alert Automatically After 2 second
-            function hideAlert() {
-                alertMessage.style.display = "none";
-            }
-            setTimeout(hideAlert, 2000);
-        } else {
-            this.buildTask();
-            this.taskInput.value = "";
-            // Hide the placeholder on any value inputs
-            placeholder.style.display = "none";
+    // append elementS to tasklist
+    taskListItem.appendChild(taskMarkContainer);
+    taskListItem.appendChild(taskValueContainer);
+    taskListItem.appendChild(taskDeleteContainer);
 
-            // Hide Alert Message
-            alertMessage.style.display = "none";
+    // add task to tasklist
+    this.taskList.appendChild(taskListItem);
+  },
 
-            // Scan the List
-            this.scanTaskList();
-        }
-    },
-    enterKey: function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            this.addTask();
-        }
-    },
-    bindEvents: function () {
-        // add click event to button
-        this.submitBtn.onclick = this.addTask.bind(this);
+  addTask: function () {
+    let taskValue = this.taskInput.value;
+    const alertMessage = document.querySelector(".alert");
+    const placeholder = document.querySelector(".placeholder");
+    if (taskValue === "") {
+      alertMessage.style.display = "block";
+      // Hide Alert Automatically After 2 second
+      function hideAlert() {
+        alertMessage.style.display = "none";
+      }
+      setTimeout(hideAlert, 2000);
+    } else {
+      // Build the task
+      this.buildTask();
+      this.taskInput.value = "";
 
-        // add enter key task textbox
-        this.taskInput.onkeypress = this.enterKey.bind(this);
-    },
-    scanTaskList: function () {
-        let taskListItem, markIcon, deleteIcon;
+      // Hide the placeholder on any value inputs
+      placeholder.style.display = "none";
 
-        // loop through all list elements
-        for (i = 0; i < this.taskListChildren.length; i++) {
-            taskListItem = this.taskListChildren[i];
+      // Hide Alert Message
+      alertMessage.style.display = "none";
 
-            // select markIcon and delete icon
-            markIcon = taskListItem.getElementsByClassName("mark")[0];
-            deleteIcon = taskListItem.getElementsByClassName("delete")[0];
-
-            // bind onclick event to the markIcon
-            markIcon.onclick = this.completeTask.bind(this, taskListItem, markIcon);
-
-            // bind onclick event to the markIcon
-            deleteIcon.onclick = this.deleteTask.bind(this, i);
-        }
-    },
-    completeTask: function (taskListItem, markIcon) {
-        markIcon.onclick =
-            markIcon.style.color = "#00b800";
-        taskListItem.style.textDecoration = "line-through";
-    },
-    deleteTask: function (i) {
-        this.taskListChildren[i].remove();
-        this.scanTaskList();
+      // Scan the List
+      this.scanTaskList();
     }
+  },
+  enterKey: function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      this.addTask();
+    }
+  },
+  bindEvents: function () {
+    // add click event to button
+    this.submitBtn.onclick = this.addTask.bind(this);
+
+    // add enter key task textbox
+    this.taskInput.onkeypress = this.enterKey.bind(this);
+  },
+  scanTaskList: function () {
+    let taskListItem, markIcon, deleteIcon;
+
+    // loop through all list elements
+    for (i = 0; i < this.taskListChildren.length; i++) {
+      taskListItem = this.taskListChildren[i];
+
+      // select markIcon and delete icon
+      markIcon = taskListItem.getElementsByClassName("checkbox")[0];
+      deleteIcon = taskListItem.getElementsByClassName("delete")[0];
+
+      // bind onclick event to the markIcon
+      markIcon.onclick = this.completeTask.bind(this, taskListItem, markIcon);
+
+      // bind onclick event to the markIcon
+      deleteIcon.onclick = this.deleteTask.bind(this, i);
+    }
+  },
+  completeTask: function (taskListItem, markIcon) {
+    if (markIcon.checked) {
+      taskListItem.style.textDecoration = "line-through";
+    } else {
+      taskListItem.style.textDecoration = "none";
+    }
+  },
+  deleteTask: function (i) {
+    this.taskListChildren[i].remove();
+    this.scanTaskList();
+  },
 };
 
 /* MADE WITH LOVE BY AL NAHIAN | Portfolio: https://alnahian2003.github.io/me | Behance: https://behance.net/alnahian2003 | Github: https://github.com/alnahian2003 */
@@ -120,40 +134,43 @@ var addTask = document.querySelector(".add svg");
 var showInput = false;
 
 addTask.addEventListener("click", function () {
-    showInput = !showInput;
-    if (showInput === true) {
-        formContainer.style.display = "block";
-        // Auto Focus on This button Click
-        document.querySelector("#input").focus();
-    } else {
-        formContainer.style.display = "none";
-    }
+  showInput = !showInput;
+  if (showInput === true) {
+    formContainer.style.display = "block";
+    // Auto Focus on This button Click
+    document.querySelector("#input").focus();
+  } else {
+    formContainer.style.display = "none";
+  }
 });
-
 
 // Display Today's Date
 var today = new Date();
 
-var date = today.getDate() + ' ' + today.toLocaleString('default', {
-    month: 'short'
-}) + ', ' + today.getFullYear();
+var date =
+  today.getDate() +
+  " " +
+  today.toLocaleString("default", {
+    month: "short",
+  }) +
+  ", " +
+  today.getFullYear();
 
 // Display Current Time with AM or PM
 function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-    console.log("Working");
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+  console.log("Working");
 }
 setInterval(function () {
-    document.querySelector(".date").innerHTML = date;
-    document.querySelector(".time").innerHTML = formatAMPM(new Date);
-}, 100)
-
+  document.querySelector(".date").innerHTML = date;
+  document.querySelector(".time").innerHTML = formatAMPM(new Date());
+}, 100);
 
 /* MADE WITH LOVE BY AL NAHIAN | Portfolio: https://alnahian2003.github.io/me | Behance: https://behance.net/alnahian2003 | Github: https://github.com/alnahian2003 */
